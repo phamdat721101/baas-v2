89af727776a99064eb39f854b7caf2b91275655e
MicheMichel <michel@vmi1108572.contaboserver.net>
2023-04-28 10:33:47 +0200

    init swagger tools

--- baas/javascript/index.js
+++ baas/javascript/index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const helmet = require('helmet');
 const routes = require('./routes');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 /**
 * Express instance
 * @public
@@ -19,13 +22,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compress());
 
 // secure apps by setting various HTTP headers
-app.use(helmet());
+// app.use(helmet());
 
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
 // mount base routers
 app.use('/', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+}));
 
 app.listen(3000, () => console.log('Server started on port 3000'));
 
